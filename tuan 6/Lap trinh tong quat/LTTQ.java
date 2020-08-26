fdb0f5836773a0dd95040d39a3e00390abf3aecb
HaiNamNguyen3012 <47827233+HaiNamNguyen3012@users.noreply.github.com>
2020-08-26 23:36:16 +0700

    Add files via upload

--- tuan 6/Lap trinh tong quat/LTTQ.java
+++ tuan 6/Lap trinh tong quat/LTTQ.java
@@ -1,1 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class LTTQ {
+    public static <T extends Comparable<T>> List<T> sortGeneric(List<T> arr) {
+        int size = arr.size();
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = i; j > 0; --j) {
+                if (arr.get(j).compareTo(arr.get(j - 1)) < 0) {
+                    T temp = arr.get(j);
+                    arr.set(j, arr.get(j - 1));
+                    arr.set(j - 1, temp);
+                }
+            }
+        }
+
+        return arr;
+    }
+
+}
